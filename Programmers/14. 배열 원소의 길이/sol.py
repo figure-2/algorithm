57756287984bc564d1d868b25dca51d167ef04b8
figure22 <pencilfoxs@gmail.com>
2023-07-31 13:53:50 +0900

    연습

--- Programmers/14. 배열 원소의 길이/sol.py
+++ Programmers/14. 배열 원소의 길이/sol.py
@@ -12,3 +12,14 @@ def solution(strlist):
 
 # def solution(strlist):
 #     return [len(i) for i in strlist]
+
+
+def solution(n):
+    answer = 0
+
+    while n > 0:
+        a, b = divmod(n, 10)
+
+        n = a
+        
+    return answer
