f0aca2d4af8d09c14918ac0c7d0a6ce58afa4f53
figure22 <pencilfoxs@gmail.com>
2023-07-31 13:50:09 +0900

    연습

---
+++ Programmers/14. 배열 원소의 길이/sol.py
@@ -0,0 +1,14 @@
+def solution(strlist):
+    answer = []
+    return answer
+
+
+def solution(strlist):
+    answer = []
+    for i in strlist:
+        answer.append(len(i))
+    return answer
+
+
+# def solution(strlist):
+#     return [len(i) for i in strlist]
---
+++ Programmers/17. 배열 뒤집기/sol.py
@@ -0,0 +1,7 @@
+def solution(num_list):
+    num_list.reverse()
+    return num_list
+
+# 풀이2
+# def solution(num_list):
+#     return num_list[::-1]
