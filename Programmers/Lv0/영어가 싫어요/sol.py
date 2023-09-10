14625773ad48e0c42deebbd12397d34b52c3b91c
figure-2 <pencilfoxs@gmail.com>
2023-09-11 00:28:04 +0900

    230910-2

--- Programmers/Lv0/영어가 싫어요/sol.py
+++ Programmers/Lv0/영어가 싫어요/sol.py
@@ -6,12 +6,12 @@ def solution(numbers):
     # dictionary에 key와 value 를 지정한다.
 
     for a, b in numbers2.items():  # dictionary 함수 중 key 와 value 를 가져오는 .items() 함수를 사용해서 a 에는 영문 / b 에는 숫자 를 입력
-        numbers = numbers.replace(a, b) # 문자열을 대치하는 .replace 함수를 써서 a 를 b 로 대치 후 다시 numbers에 저장
+        numbers = numbers.replace(a, str(b)) # 문자열을 대치하는 .replace 함수를 써서 a 를 b 로 대치 후 다시 numbers에 저장
 
     return int(numbers)
 
 
-# enumerate() 라는 함수도 있네 
+# enumerate() 라는 함수도 있네 들어본적은 있는데 기억이... 이 함수를 쓰면 리스트에서 인덱스 와 객체(?)를 순서대롤 나열???
 
 
 # for entry in enumerate(['A', 'B', 'C']):
